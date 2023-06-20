5740a0d80d4d85e346695eddf434ff5e166cd94c
Alex Lablaiks <alex.lablaiks@sap.com>
2023-06-20 17:47:24 -0500

    Format and Use New Classes

--- MockDataFlatPromisesServicePage.tsx
+++ MockDataFlatPromisesServicePage.tsx
@@ -1,118 +1,65 @@
-import React, {Component} from 'react';
-import {Alert, Button, FlatList, Text, TextStyle, View} from 'react-native';
-import {ObjectCellView} from './ObjectCellView';
+import React, { Component } from 'react';
+import { Alert, Button, FlatList, Text, TextStyle, View } from 'react-native';
 
-import {IObjectCell} from './IObjectCell';
-import {getObjectCells as get1kObjectCells} from './MockDataServiceObjectCells1k';
-import {getObjectCells as get10kObjectCells} from './MockDataServiceObjectCells10k';
+import { type IObjectCell } from './IObjectCell';
+import { ObjectCellView } from './ObjectCellView';
+import { MockDataFlatPromisesService } from './MockDataFlatPromisesService';
 
 interface Props {
-  onGoBack?: () => void;
+	onGoBack?: () => void;
 }
 
 interface State {
-  data: Array<IObjectCell>;
+	objectCellIter: number;
+}
+
+function onReport(msg: string): void {
+	Alert.alert('Measured Time', msg);
 }
 
 export class MockDataFlatPromisesServicePage extends Component<Props, State> {
-  state: {data: Array<IObjectCell>} = {data: []};
-  private toUpper = true;
-  private objectCells: Array<IObjectCell> = [];
+	state: State = { objectCellIter: 0 };
+	private _mockDataFlatPromisesService: MockDataFlatPromisesService = new MockDataFlatPromisesService(onReport);
 
-  private fieldValueToUpper(objectCell: IObjectCell, key: string) {
-    if (!(key in objectCell)) {
-      return;
-    }
-    const value = objectCell[key];
-    objectCell[key] = this.toUpper ? value.toUpperCase() : value.toLowerCase();
-  }
+	private onFormat(): void {
+		this._mockDataFlatPromisesService.format().then(() => {
+			this.setState({ objectCellIter: this.state.objectCellIter++ });
+		});
+	}
 
-  private onFormat(): void {
-    const start = new Date();
-    const promiseArray: Array<Promise<void>> = [];
+	private onGet1kObjectCells(): void {
+		this._mockDataFlatPromisesService.populate1kObjectCells();
+	}
 
-    for (const objectCell of this.objectCells) {
-      for (const key of Object.keys(objectCell)) {
-        promiseArray.push(
-          new Promise(resolve => {
-            this.fieldValueToUpper(objectCell, key);
-            resolve();
-          }),
-        );
-      }
-    }
+	private onGet10kObjectCells(): void {
+		this._mockDataFlatPromisesService.populate10kObjectCells();
+	}
 
-    this.toUpper = !this.toUpper;
-    Promise.all(promiseArray).then(() => {
-      const end = new Date();
-      const durationMs = end.getTime() - start.getTime();
-      console.log(
-        `formatted ${this.objectCells.length} object cells in ${durationMs} ms`,
-      );
+	public override render(): JSX.Element {
+		const titleStyle: TextStyle = {
+			textAlign: 'center',
+			fontSize: 40,
+		};
 
-      Alert.alert(
-        'Measured Time',
-        `formatted ${this.objectCells.length} object cells in ${durationMs} ms`,
-      );
-
-      this.setState({data: this.objectCells});
-    });
-  }
-
-  private async getObjectCells(
-    getObjectCells: () => Array<IObjectCell>,
-  ): Promise<void> {
-    const start = new Date();
-    this.objectCells = getObjectCells();
-    const end = new Date();
-    const durationMs = end.getTime() - start.getTime();
-
-    console.log(
-      `populated ${this.objectCells.length} object cells in ${durationMs} ms`,
-    );
-
-    this.setState({data: this.objectCells});
-  }
-
-  private onGet1kObjectCells(): void {
-    this.getObjectCells(get1kObjectCells);
-  }
-
-  private onGet10kObjectCells(): void {
-    this.getObjectCells(get10kObjectCells);
-  }
-
-  public override render(): JSX.Element {
-    const titleStyle: TextStyle = {
-      textAlign: 'center',
-      fontSize: 40,
-    };
-
-    return (
-      <View>
-        <Text style={titleStyle}>Mock Data with Flat Promises</Text>
-        <Button
-          disabled={!(this.props.onGoBack instanceof Function)}
-          title="Main Screen"
-          onPress={() => {
-            if (this.props.onGoBack) this.props.onGoBack();
-          }}
-        />
-        <Button
-          title="Get 1K Object Cells"
-          onPress={() => this.onGet1kObjectCells()}
-        />
-        <Button
-          title="Get 10K Object Cells"
-          onPress={() => this.onGet10kObjectCells()}
-        />
-        <Button title="Format" onPress={() => this.onFormat()} />
-        <FlatList
-          data={this.state.data}
-          renderItem={ObjectCellView.renderItem}
-          keyExtractor={(item: IObjectCell) => item._id}
-        />
-      </View>
-    );
-  }
+		return (
+			<View>
+				<Text style={titleStyle}>Mock Data with Flat Promises</Text>
+				<Button
+					disabled={!(this.props.onGoBack instanceof Function)}
+					title="Main Screen"
+					onPress={() => {
+						if (this.props.onGoBack) this.props.onGoBack();
+					}}
+				/>
+				<Button title="Get 1K Object Cells" onPress={() => this.onGet1kObjectCells()} />
+				<Button title="Get 10K Object Cells" onPress={() => this.onGet10kObjectCells()} />
+				<Button title="Format" onPress={() => this.onFormat()} />
+				<FlatList
+					data={this._mockDataFlatPromisesService.objectCells}
+					renderItem={ObjectCellView.renderItem}
+					keyExtractor={(item: IObjectCell) => item._id}
+				/>
+			</View>
+		);
+	}
 }
--- MockDataNestedPromisesServicePage.tsx
+++ MockDataNestedPromisesServicePage.tsx
@@ -1,132 +1,65 @@
-import React, {Component} from 'react';
-import {Alert, Button, FlatList, Text, TextStyle, View} from 'react-native';
+import React, { Component } from 'react';
+import { Alert, Button, FlatList, Text, TextStyle, View } from 'react-native';
 
-import {ObjectCellView} from './ObjectCellView';
-
-import {IObjectCell} from './IObjectCell';
-import {getObjectCells as get1kObjectCells} from './MockDataServiceObjectCells1k';
-import {getObjectCells as get10kObjectCells} from './MockDataServiceObjectCells10k';
+import { type IObjectCell } from './IObjectCell';
+import { ObjectCellView } from './ObjectCellView';
+import { MockDataNestedPromisesService } from './MockDataNestedPromisesService';
 
 interface Props {
-  onGoBack?: () => void;
+	onGoBack?: () => void;
 }
 
 interface State {
-  data: Array<IObjectCell>;
+	objectCellIter: number;
+}
+
+function onReport(msg: string): void {
+	Alert.alert('Measured Time', msg);
 }
 
 export class MockDataNestedPromisesServicePage extends Component<Props, State> {
-  state: {data: Array<IObjectCell>} = {data: []};
-  private toUpper = true;
+	state: State = { objectCellIter: 0 };
+	private _mockDataNestedPromisesService = new MockDataNestedPromisesService(onReport);
 
-  private formatKeyValue(
-    key: string,
-    value: string,
-  ): Promise<[string, string]> {
-    return new Promise(resolve => {
-      const resolvedValue = this.toUpper
-        ? value.toUpperCase()
-        : value.toLowerCase();
-      resolve([key, resolvedValue]);
-    });
-  }
+	private onGet1kObjectCells(): void {
+		this._mockDataNestedPromisesService.populate1kObjectCells();
+	}
 
-  private formatObjectCell(objectCell: IObjectCell): Promise<IObjectCell> {
-    const fieldPromises: Array<Promise<[string, string]>> = [];
+	private onGet10kObjectCells(): void {
+		this._mockDataNestedPromisesService.populate10kObjectCells();
+	}
 
-    for (const key of Object.keys(objectCell)) {
-      fieldPromises.push(this.formatKeyValue(key, objectCell[key]));
-    }
+	private onFormat(): void {
+		this._mockDataNestedPromisesService.format().then(() => {
+			this.setState({ objectCellIter: this.state.objectCellIter++ });
+		});
+	}
 
-    return new Promise(resolve => {
-      Promise.all(fieldPromises).then((tuples: [string, string][]) => {
-        const objectCell: Record<string, string> = {};
-        for (const tuple of tuples) {
-          objectCell[tuple[0]] = tuple[1];
-        }
-        resolve(objectCell as IObjectCell);
-      });
-    });
-  }
+	public override render(): JSX.Element {
+		const titleStyle: TextStyle = {
+			textAlign: 'center',
+			fontSize: 40,
+		};
 
-  private async getObjectCells(
-    getObjectCells: () => Array<IObjectCell>,
-  ): Promise<void> {
-    const start = new Date();
-    const objectCells = getObjectCells();
-    const end = new Date();
-    const durationMs = end.getTime() - start.getTime();
-
-    console.log(
-      `populated ${objectCells.length} object cells in ${durationMs} ms`,
-    );
-    this.setState({data: objectCells});
-  }
-
-  private onGet1kObjectCells(): void {
-    this.getObjectCells(get1kObjectCells);
-  }
-
-  private onGet10kObjectCells(): void {
-    this.getObjectCells(get10kObjectCells);
-  }
-
-  private onFormat(): void {
-    const start = new Date();
-
-    const objectCellPromises: Array<Promise<IObjectCell>> = [];
-    for (const objectCell of this.state.data) {
-      objectCellPromises.push(this.formatObjectCell(objectCell));
-    }
-
-    this.toUpper = !this.toUpper;
-    Promise.all(objectCellPromises).then((objectCells: Array<IObjectCell>) => {
-      const end = new Date();
-      const durationMs = end.getTime() - start.getTime();
-      console.log(
-        `formatted ${objectCells.length} object cells in ${durationMs} ms`,
-      );
-
-      Alert.alert(
-        'Measured Time',
-        `formatted ${objectCells.length} object cells in ${durationMs} ms`,
-      );
-
-      this.setState({data: objectCells});
-    });
-  }
-
-  public override render(): JSX.Element {
-    const titleStyle: TextStyle = {
-      textAlign: 'center',
-      fontSize: 40,
-    };
-
-    return (
-      <View>
-        <Text style={titleStyle}>Mock Data with Nested Promises</Text>
-        <Button
-          disabled={!(this.props.onGoBack instanceof Function)}
-          title="Main Screen"
-          onPress={() => {
-            if (this.props.onGoBack) this.props.onGoBack();
-          }}
-        />
-        <Button
-          title="Get 1K Object Cells"
-          onPress={() => this.onGet1kObjectCells()}
-        />
-        <Button
-          title="Get 10K Object Cells"
-          onPress={() => this.onGet10kObjectCells()}
-        />
-        <Button title="Format" onPress={() => this.onFormat()} />
-        <FlatList
-          data={this.state.data}
-          renderItem={ObjectCellView.renderItem}
-          keyExtractor={(item: IObjectCell) => item._id}
-        />
-      </View>
-    );
-  }
+		return (
+			<View>
+				<Text style={titleStyle}>Mock Data with Nested Promises</Text>
+				<Button
+					disabled={!(this.props.onGoBack instanceof Function)}
+					title="Main Screen"
+					onPress={() => {
+						if (this.props.onGoBack) this.props.onGoBack();
+					}}
+				/>
+				<Button title="Get 1K Object Cells" onPress={() => this.onGet1kObjectCells()} />
+				<Button title="Get 10K Object Cells" onPress={() => this.onGet10kObjectCells()} />
+				<Button title="Format" onPress={() => this.onFormat()} />
+				<FlatList
+					data={this._mockDataNestedPromisesService.objectCells}
+					renderItem={ObjectCellView.renderItem}
+					keyExtractor={(item: IObjectCell) => item._id}
+				/>
+			</View>
+		);
+	}
 }
