7785c4c4ae31d18af4e5abb3258cee30d65fc2dc
Alex Lablaiks <alex.lablaiks@sap.com>
2023-06-21 14:34:30 -0500

    Remove duplicate files

--- IObjectCell.tsx
+++
@@ -1,9 +0,0 @@
-export interface IObjectCell extends Record<string, string> {
-	_id: string;
-	Title: string;
-	Subhead: string;
-	Footnote: string;
-	Description: string;
-	Status: string;
-	Substatus: string;
-}
